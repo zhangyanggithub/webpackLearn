0f0d3677fb3f8e770d8a3eebd72f61348002b3c1
zhangyang66 <zhangyang66@meituan.com>
2019-02-25 20:20:34 +0800

    loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './img.jpg';
+
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  let img = new Image();
+  img.src = Icon;
+  
+  element.appendChild(img);
   return element;
 }
+
 const child = component();
 document.body.appendChild(child);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 };
